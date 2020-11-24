3bb1c62a9e45490ccea0d8d084d4c9c91645e330
Alice Golter <inkflight@gmail.com>
2020-11-23 20:18:03 -0600

    updated generateMD and index and gitignore

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,74 @@
-// array of questions for user
-const questions = [
+// the introduction of different modules
 
+const fs = require ('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const axios = require('axios');
+
+// this is a connection to the document generateMarkdown.js
+const generateMarkdown = require('./generateMarkdown')
+
+// JSON for where the info can be changed depending on the project
+const questions = [
+ {
+    //  github username
+     type: 'input',
+     name: 'github',
+     message: 'What is your Github Username?'
+ },
+ {
+    //  email
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?'
+ },
+ {
+    //  title of project the README is for
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+},
+ {
+    // description of project README is for
+    type: 'input',
+    name: 'description',
+    message: 'Describe your project here'
+},
+ {
+    //  licensure that the project has
+    type: 'list',
+    name: 'license',
+    message: 'What kind of licensure should your project have?',
+    choices: ['MIT', 'APACHE 2.0', 'GPK 3.0', 'BSD 3', 'None']
+},
+{
+    // installation instructions for the project
+    type: 'input',
+    name: 'installation',
+    message: 'What commands should be run to install dependencies?',
+    default: 'npm i'
+},
+{
+    //  commands used in order to run testing
+    type: 'input',
+    name: 'test',
+    message: 'What commands should be run to test this program?',
+    default: 'npm test'
+}
 ];
 
-// function to write README file
+// function to write the actual README file
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
-// function to initialize program
+// function to initialize the program in question using inquirer to create a CLI
 function init() {
-
+    inquirer.prompt(questions).then((inquirerResponses) => {
+        // this is the console.log indicating the README actually being written/generated
+        console.log('Generating README...');
+        writeToFile('README.md', generateMarkdown({...inquirerResponses}));
+    });
 }
 
 // function call to initialize program
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,81 @@
+// this is what is generated as far as README written out material goes
+
+// this actually helps the readme nab the license itself, show the licensure image, and fill in the license section of the README as needed.
+function renderLicenseBadge(license) {
+  if (licen !== 'None') {
+    return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`
+    }
+    return "";
+  }
+
+  function renderLicenseLink(license) {
+    if (license !== 'None') {
+      return `\n* [License](#license)\n`;
+    }
+    return "";
+  }
+
+  function renderLicenseSection(license) {
+    if (license !== 'None') {
+      return `## License
+      
+      This project is licensed under the ${license} license.`;
+    }
+      return '';
+  }
+
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+*[Installation](#installation)
+*[Usage](#usage)
+${renderLicenseLink(data.license)}
+*[Contributing](#contributing)
+*[Tests](#tests)
+*[Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+\`\`\`
+${data.installation}
+\`\`\`
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+To run tests, run the following command:
+
+\`\`\`
+${data.test}
+\`\`\`
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at ${
+  data.email
+}. You can find more of my work at [${data.github}](https://github.com/${
+  data.github
+}/).
 
 `;
 }
